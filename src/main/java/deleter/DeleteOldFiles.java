00596d84825ec45e6b1dc7317f2e1c146b91fdd9
Sergiy.K <s.konoplyaniy@gmail.com>
2017-07-04 10:56:49 +0300

    Initial commit

---
+++ src/main/java/deleter/DeleteOldFiles.java
@@ -0,0 +1,60 @@
+package deleter;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created on 12-Jun-17.
+ */
+public class DeleteOldFiles {
+    static ArrayList<String> folders = new ArrayList<>();
+    static long currentTime = System.currentTimeMillis();
+    static int count = 0;
+    static long space = 0;
+
+    static {
+        folders.add("C:\\Automation\\Screenshot");
+        folders.add("C:\\Automation\\Tmp");
+        folders.add("C:\\Users\\" + System.getProperty("user.name") + "\\AppData\\Local\\Temp");
+    }
+
+    public static void main(String[] args) {
+        for (String folder : folders) {
+            deleteFiles(new File(folder));
+        }
+        System.out.println("Deleted " + count + " files");
+        System.out.println(space / (1024 * 1024) + " Mb");
+    }
+
+
+    public void deleteOldFiles() {
+        for (String folder : folders) {
+            deleteFiles(new File(folder));
+        }
+        System.out.println("Deleted " + count + " files");
+        System.out.println(space / (1024 * 1024) + " Mb");
+    }
+
+    private static void deleteFiles(File file) {
+        if (file.isDirectory()) {
+            if (file.listFiles() == null) {
+                file.delete();
+                return;
+            }
+            for (File f : file.listFiles()) {
+                deleteFiles(f);
+            }
+        } else {
+            long lastModified = currentTime - file.lastModified();
+            if (lastModified > 1000 * 60 * 60 * 24 * 7) {
+                long fileSize = file.length();
+                boolean isDeleted = file.delete();
+                System.out.println(file.getAbsoluteFile() + " - " + (isDeleted ? "deleted" : "not deleted"));
+                if (isDeleted) {
+                    count++;
+                    space = space + fileSize;
+                }
+            }
+        }
+    }
+}
